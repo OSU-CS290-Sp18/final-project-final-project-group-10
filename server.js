61a7b95b23b5177853b2b662fbd81633fc148b76
emilymd <32113892+emilymd@users.noreply.github.com>
2018-06-12 13:48:35 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 /*server side java*/
-var express = require('express');
 var path = require('path');
+var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
-var postData = require('./postData');
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = "mongodb://" +
+  mongoUsername + ":" + mongoPassword + "@" +
+  mongoHost + ":" + mongoPort + "/" + mongoDBName;
+
+var mongoDB = null;
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+var postData = require('./postData');
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
+//app.use(express.static('public'));
 
-app.get('/', function(req,res){
+app.get('/', function(req,res,next){
   res.status(200).render('postPage', {
     data: postData}
   );
 });
 
-app.get('*', function(req,res){
+app.get('*', function(req,res,next){
   res.status(404).render('404');
 })
 // app.get('/', function(req, res) {
@@ -34,7 +49,12 @@ app.get('*', function(req,res){
 // app.get('*', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/public/404.html'));
 // });
-
-app.listen(port, function () {
-    console.log("== Server is listening on port", port);
-  });
+MongoClient.connect(mongoURL, function(err, client){
+  if (err){
+    throw err;
+  }
+  mongoDB = client.db(mongoDBName);
+  app.listen(port, function () {
+      console.log("== Server is listening on port", port);
+    });
+})
